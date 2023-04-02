203c517c2bef4cba06fce9859291bfa85dc667c2
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-02 06:19:48 +0200

    converting all html files into pug files

--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,9 @@ const rootDir = require('../utils/path');
 const products = [];
 
 router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
+    res.render('add-product',{PageTitle:'Add Product' , path:'/admin/add-product'})
 });
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
     products.push({title:req.body.title})
     res.redirect('/');
 });
