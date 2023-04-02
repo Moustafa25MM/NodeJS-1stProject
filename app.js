2f9025d0ff0974e1946524a3853e66c8ec3813b9
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-02 12:00:29 +0200

    adding error controller

--- app.js
+++ app.js
@@ -13,13 +13,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 
-app.use((req, res, next) => {
-    res.status(404).render('404',{pageTitle:'Page not found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
