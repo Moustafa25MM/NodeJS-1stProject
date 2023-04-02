14164f703da86345945f013d1839fad8c0810fba
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-02 12:15:40 +0200

    adding product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,16 @@
 
-const products = [];
-
+const Product = require('../models/product');
 
 exports.getAddProduct = (req,res,next)=>{
     res.render('add-product',{PageTitle:'Add Product' , path:'/admin/add-product'})
 };
 
 exports.postAddProduct = (req,res,next)=>{
-    products.push({title:req.body.title})
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req,res,next)=> {
-    res.render('shop',{prods:products , pageTitle:'Shop' ,path:'/'});
+    res.render('shop',{prods:Product.fetchAll() , pageTitle:'Shop' ,path:'/'});
 };
