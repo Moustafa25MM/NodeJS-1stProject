613bd9481a1a6c3263bc7f64a8ce46610006a4e4
moustafa mahmoud <mostafa09mahmoud@gmail.com>
2023-04-02 06:00:37 +0200

    added shop.pug file instead of Html file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views','views');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
